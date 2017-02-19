a130a9b883c9597f1ad0258ba5b54ba30721e4e5
Shah <SHAHS@eu.hasbro.com>
2017-02-19 19:47:13 +0000

    Updated 1

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
           //  badRemove: '=',
            onRemove: '&'
           },
-          controller: NarrowItDownDirectiveController,
+          controller: NarrowItDownController,
           controllerAs: 'searchDirCtrl',
           bindToController: true
           //link: shoppingListDirectiveLink
@@ -25,48 +25,37 @@
       }
 
 
-      function NarrowItDownDirectiveController() {
-        /*
-        var searchDirCtrl = this;
-
-        searchDirCtrl.onRemove = function () {
-        //  for (var i = 1; i < list.items.length; i++) {
-            var name = list.found[list.found.length-1].description;
-            if (name.toLowerCase().indexOf("tofu") !== -1) {
-              return true;
-            }
-
-          //}
-
-          return false;
-
-        };
-        */
-      }
-
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
 
   var searchController = this;
-
+  searchController.searchTerm = ' ';
 
   searchController.getMenuItems = function (searchTerm) {
-    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+    if (searchTerm != '' )
+     {
+       searchController.showMessage = '';
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
 
-    promise.then(function (response){
-      var i = 0;
-      searchController.found = [];
-      for (i=0;i<=response.data.menu_items.length-1;i++){
+        promise.then(function (response){
+          var i = 0;
+          searchController.found = [];
+          for (i=0;i<=response.data.menu_items.length-1;i++){
 
-        if (response.data.menu_items[i].description.toLowerCase().indexOf(searchTerm) !== -1){
-          searchController.found.push(response.data.menu_items[i]);
-        }
+            if (response.data.menu_items[i].description.toLowerCase().indexOf(searchTerm) !== -1){
+              searchController.found.push(response.data.menu_items[i]);
+            }
+          }
+        })
+        .catch(function(error){
+          console.log("Something went wrong !");
+        });
       }
-    })
-    .catch(function(error){
-      console.log("Something went wrong !");
-    });
+    else {
+        searchController.showMessage="Nothing Found!";
+        searchController.found = [];
+    }
   }
 
   searchController.removeItem = function(itemIndex) {
